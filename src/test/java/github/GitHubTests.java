8c1f57c35c03f674aaee36758481797a729e9681
VlTemadev <cfpro.tema@mail.ru>
2024-02-27 13:57:56 +0600

    add new github test

--- src/test/java/github/GitHubTests.java
+++ src/test/java/github/GitHubTests.java
@@ -1,14 +1,18 @@
 package github;
 
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class GitHubTests {
     String BASE_URL = "https://github.com/";
+    String SELENIDE_URL = "https://github.com/selenide/selenide";
     String VALUE = "selenide";
     String SEARCH_TEXT = "selenide/selenide";
+    String AUTHOR_NAME = "Andrei Solntsev";
 
     @Test
     void ShouldSelenideTextInTheTop(){
@@ -18,5 +22,16 @@ public class GitHubTests {
         $(".search-match").shouldHave(text(SEARCH_TEXT)).click();
         $("[data-turbo-frame='repo-content-turbo-frame']").shouldHave(text(VALUE));
 
+    }
+
+    @Test
+    void ShouldHaveTextInFirstAuthorBlock(){
+        open(SELENIDE_URL);
+        $("div.Layout-sidebar").$(byText("Contributors")).closest(".BorderGrid-cell")
+                .$("ul li:first-child")
+                .hover();
+        $(".Popover-message").shouldHave(text("Andrei Solntsev"));
+
+        sleep(5000);
     }
 }
